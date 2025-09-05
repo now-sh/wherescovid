1343efdeba42335dd5347e67207ecf01645fdd73
casjay <git-admin@casjaysdev.pro>
2025-09-05 18:48:36 -0400

    📃 Deployed: src/main.js 📃
    Deployed: src/main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/dracula-theme.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
